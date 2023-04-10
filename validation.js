2d62f665b8280ef1adf1fb6c3a03613c4e7c8514
Bin Nguyễn <nguyenybin2015@gmail.com>
2023-04-11 00:27:03 +0700

    first commit

---
+++ validation.js
@@ -0,0 +1,14 @@
+const { check } = require('express-validator');
+
+//signup 
+exports.signupValidation = [
+    check('name', 'Name is requied').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail().normalizeEmail({ gmail_remove_dots: true }),
+    check('password', 'Password must be 6 or more characters').isLength({ min: 6 })
+]
+
+//login user
+exports.loginValidation = [
+     check('email', 'Please include a valid email').isEmail().normalizeEmail({ gmail_remove_dots: true }),
+     check('password', 'Password must be 6 or more characters').isLength({ min: 6 })
+]
