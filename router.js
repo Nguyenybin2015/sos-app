e23c5fbe961fd57d6d36c090c0c4d06c98113f15
Bin Nguyễn <nguyenybin2015@gmail.com>
2023-04-14 10:01:51 +0700

    latest 2fa

--- router.js
+++ router.js
@@ -5,50 +5,7 @@ const { signupValidation, loginValidation } = require('./validation');
 const { validationResult, check } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const twoFactor = require('./twoFactor.js');
 const Speakeasy = require("speakeasy");
-
-// const qrcode = require('qrcode');
-// var secret = Speakeasy.generateSecret({ name: "bin" });
-// console.log(secret);
-// var verify = Speakeasy.totp.verify({
-//     secret: 'HBDWIYSQMNFWEMRVKV4SIQLRPNHFILZZHFNTE43WFAWH2XJQEVQQ',
-//     encoding: "base32",
-//     token: "488862"
-// });
-// console.log(verify);
-// router.post('/verify', (req, res) => {
-//     res.send({
-//         "valid": Speakeasy.totp.verify({
-//             secret: 'GASVAQJRLVOW6R2KJRGTAVRWI5NF2NROHJQXOVSAPVEW4I2TGJUQ',
-//             encoding: "base32",
-//             token: req.body.token,
-//         })
-//     });
-// });
-// router.post("/totp-secret", (req, res, next) => {
-//     var secret = Speakeasy.generateSecret({ length: 20 });
-//     res.send({ "secret": secret.base32 });
-// });
-// router.post("/totp-generate", (req, res, next) => {
-//     res.send({
-//         "token": Speakeasy.totp({
-//             secret: req.body.secret,
-//             encoding: "base32"
-//         }),
-//         "remaining": (30 - Math.floor((new Date()).getTime() / 1000.0 % 30)) // time 30 seconds
-//     });
-// });
-// router.post("/totp-validate", (req, res, next) => {
-//     res.send({
-//         "valid": Speakeasy.totp.verify({
-//             secret: req.body.secret,
-//             encoding: "base32",
-//             token: req.body.token,
-//             window: 0
-//         })
-//     });
-// });
 
 
 router.post('/register', signupValidation, (req, res, next) => {
@@ -140,34 +97,33 @@ router.post('/login', (req, res, next) => {
                             // secret: secret.base32,
                             encoding: "base32",
                         });
-                        if (token != null) {
-                            return res.send({
-                                token: token
-                            });
-                        }
-                        // res.status(200).json({ otp: token });
-                        // let remaining = 30 - Math.floor((new Date().getTime() / 1000.0) % 30);
-                        // res.render("token", { token, remaining });
                         console.log(token);
-                        var verify = Speakeasy.totp.verify({
-                            secret: 'JFTDSLBBG42GSWCIOQZU4NBSEZ4XUSLOGVTTCSSMIA2HIU2IKMTA',
-                            encoding: "base32",
-                            token: req.body.TokenClient
-                        });
-                        console.log(verify);
-                        if (verify) {
-                            return res.status(200).send({
-                                msg: 'Logged in!',
-                                user: result[0]
+                        console.log(req.body.TokenClient);
+                        try {
+                            var verify = Speakeasy.totp.verify({
+                                secret: 'JFTDSLBBG42GSWCIOQZU4NBSEZ4XUSLOGVTTCSSMIA2HIU2IKMTA',
+                                encoding: "base32",
+                                token: req.body.TokenClient
                             });
-                        }
-                        else {
-                            return res.status(401).send({
-                                msg: 'Please include a valid OTP'
-                            })
+                            console.log(verify);
+                            if (verify) {
+                                return res.status(200).send({
+                                    msg: 'Logged in!',
+                                    user: result[0]
+                                });
+                            }
+                            else {
+                                return res.Pleasestatus(401).send({
+                                    msg: ' include a valid OTP'
+                                })
+                            }
+                        } catch (verify) {
+                            return res.send({
+                                TokenClient: token
+                            });
+                            
                         }
                     }
-
                     return res.status(401).send({
                         msg: 'Username or password is incorrect!'
                     });
