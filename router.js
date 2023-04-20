871b557386e42aff5059011cbcf6cbfef56ba5e6
Bin Nguyễn <nguyenybin2015@gmail.com>
2023-04-20 10:33:43 +0700

    update profile latest

--- router.js
+++ router.js
@@ -45,9 +45,13 @@ router.post('/register', signupValidation, (req, res, next) => {
                                             status: '500 Internal Server Error'
                                         });
                                     } else {
-                                        res.status(201).send({
-                                            msg: 'The user has been registerd with us!',
-                                            status: '201 Created'
+                                        db.query(`SELECT id FROM users WHERE email = ${db.escape(req.body.email)}`, (err, id) => {
+                                            // console.log(id)
+                                            res.status(201).send({
+                                                msg: 'The user has been registerd with us!',
+                                                user: id[0],
+                                                status: '201 Created'
+                                            });
                                         });
                                     }
                                 });
@@ -78,9 +82,9 @@ router.post('/register', signupValidation, (req, res, next) => {
     );
 });
 
+var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 router.post('/login', (req, res, next) => {
     // check email valid or invalid
-    var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
     if (emailRegex.test(req.body.email)) {
         next();
     }
@@ -129,9 +133,6 @@ router.post('/login', (req, res, next) => {
                             secret: process.env.SECRET_TOKEN,
                             encoding: "base32",
                         });
-                        // console.log(process.env.SECRET_TOKEN)
-                        // console.log(token);
-                        // console.log(req.body.TokenClient);
                         try {
                             var verify = Speakeasy.totp.verify({
                                 secret: process.env.SECRET_TOKEN,
@@ -186,79 +187,12 @@ router.get('/users/:userId', (req, res, next) => {
     });
 
 });
-// router.put('/users/:userId', (req, res, next) => {
-//     const userId = req.params.userId;
 
-//     db.query(`SELECT * FROM users, profile_user WHERE id = ${userId}`, (err, result) => {
-//         console.log({ result })
-//         if (err) {
-//             console.log(err);
-//             res.status(500).send('Internal server error');
-//         } else {
-//             if (result.length === 0) {
-//                 res.status(404).send('User not found');
-//             } else {
-//                 const user = result[0];
-
-//                 if (req.body.name) user.name = req.body.name;
-//                 console.log(req.body.name);
-//                 // if (req.body.email) user.email = req.body.email;
-//                 // if (req.body.password) user.password = req.body.password;
-//                 // if (req.body.phone) user.phone = req.body.phone;
-//                 if (req.body.avatar) user.avatar = req.body.avatar;
-//                 if (req.body.lock_deposits) user.lock_deposits = req.body.lock_deposits;
-//                 if (req.body.lock_withdrawals) user.lock_withdrawals = req.body.lock_withdrawals;
-//                 if (req.body.maintenance) user.maintenance = req.body.maintenance;
-//                 if (req.body.close_system) user.close_system = req.body.close_system;
-
-//                 // Update the user's profile information in the database
-//                 console.log(user.name)
-//                 console.log(user.avatar)
-//                 console.log(user.lock_deposits)
-//                 console.log(user.lock_withdrawals)
-//                 console.log(user.maintenance)
-//                 console.log(user.close_system)
-//                 console.log(user.id)
-//                 db.query(`UPDATE users, profile_user SET name = '${user.name}', avatar = ${user.avatar}, lock_deposits = ${user.lock_deposits}, lock_withdrawals = ${user.lock_withdrawals}, maintenance = ${user.maintenance}, close_system = ${user.close_system}  WHERE id = ${user.id}`, (err, result) => {
-//                     if (err) {
-//                         console.log(err);
-//                         res.status(500).send('Internal server error');
-//                     } else {
-//                         if (result.affectedRows === 0) {
-//                             res.status(400).send('Invalid request');
-//                         } else {
-//                             res.status(200).send('Profile updated successfully');
-//                             // console.log(userId)
-//                             db.query(`SELECT * FROM users, profile_user WHERE id = ${userId}`, (err, user) => { console.log({ user }) });
-
-//                         }
-//                     }
-//                 });
-//                 // db.query(`UPDATE users, profile_user SET name = ?, avatar = ?, lock_deposits = ?, lock_withdrawals = ?, maintenance = ?, close_system = ?  WHERE id = ?`, [user.name, user.avatar, user.lock_deposits, user.lock_withdrawals, user.maintenance, user.close_system, userId], (err, result) => {
-//                 //     if (err) {
-//                 //         console.log(err);
-//                 //         res.status(500).send('Internal server error');
-//                 //     } else {
-//                 //         if (result.affectedRows === 0) {
-//                 //             res.status(400).send('Invalid request');
-//                 //         } else {
-//                 //             res.status(200).send('Profile updated successfully');
-//                 //             // console.log(userId)
-//                 //             db.query(`SELECT * FROM users, profile_user WHERE id = ${userId}`, (err, user) => { console.log({user})});
-
-//                 //         }
-//                 //     }
-//                 // });
-//             }
-//         }
-//     });
-
-// });
 router.put('/users/:userId', (req, res, next) => {
     const userId = req.params.userId;
 
     // Check if the user exists
-    db.query(`SELECT * FROM users WHERE id = ${userId}`, (err, result) => {
+    db.query(`SELECT * FROM users INNER JOIN profile_user ON users.email = profile_user.email WHERE users.id = ${userId} `, async (err, result) => {
         if (err) {
             console.log(err);
             res.status(500).send('Internal server error');
@@ -268,29 +202,92 @@ router.put('/users/:userId', (req, res, next) => {
             } else {
                 // Update the user's profile information
                 const user = result[0];
-                console.log(req.body);
+                // console.log(req.body);
 
-                user.name = req.body.name;
-                user.avatar = req.body.avatar;
-                user.lock_deposits = req.body.lock_deposits;
-                user.lock_withdrawals = req.body.lock_withdrawals;
-                user.maintenance = req.body.maintenance;
-                user.close_system = req.body.close_system;
-
-                // Update the user's profile information in the database
-                db.query(`UPDATE users, profile_user SET name = ?, avatar = ?, lock_deposits = ?, lock_withdrawals = ?, maintenance = ?, close_system = ?`, [user.name, user.avatar, user.lock_deposits, user.lock_withdrawals, user.maintenance, user.close_system],
-                    (err, result) => {
+                // user.password = await bcrypt.hash(req.body.password, 10);
+                if (req.body.name) user.name = req.body.name;
+                if (req.body.avatar) user.avatar = req.body.avatar;
+                if (req.body.lock_deposits) user.lock_deposits = req.body.lock_deposits;
+                if (req.body.lock_withdrawals) user.lock_withdrawals = req.body.lock_withdrawals;
+                if (req.body.maintenance) user.maintenance = req.body.maintenance;
+                if (req.body.close_system) user.close_system = req.body.close_system;
+                if(req.body.email){
+                    if (emailRegex.test(req.body.email)) {
+                        user.email= req.body.email;
+                    }
+                    else {
+                        console.log("Email or password is invalid");
+                            res.status(401).send({
+                            msg: 'Please include a valid email and password',
+                            status: '401 Unauthorized'
+                        });
+                    }
+                }
+                db.query(`UPDATE users INNER JOIN profile_user ON users.id = profile_user.id SET name = ?, profile_user.email = ?, avatar = ?, lock_deposits = ?, lock_withdrawals = ?, maintenance = ?, close_system = ? WHERE users.id = ${userId}`, [user.name, user.email, user.avatar, user.lock_deposits, user.lock_withdrawals, user.maintenance, user.close_system],
+                    async (err, result) => {
                         if (err) {
                             console.log(err);
                             res.status(500).send('Internal server error');
                         } else {
-                            if (result.affectedRows === 0) {
-                                res.status(400).send('Invalid request');
-                            } else {
+                            if (req.body.password) {
+                                var token = Speakeasy.totp({
+                                    secret: process.env.SECRET_TOKEN,
+                                    encoding: "base32",
+                                });
+                                try {
+                                    var verify = Speakeasy.totp.verify({
+                                        secret: process.env.SECRET_TOKEN,
+                                        encoding: "base32",
+                                        token: req.body.TokenClient
+                                    });
+                                    if (verify) {
+                                        user.password = await bcrypt.hash(req.body.password, 10);
+                                        db.query(`UPDATE users INNER JOIN profile_user ON users.id = profile_user.id SET password = ? WHERE users.id = ${userId}`, [user.password], () => {
+                                            db.query(`SELECT * FROM users INNER JOIN profile_user ON users.id = profile_user.id WHERE users.id = ${userId} `, (err, profile) => {
+                                                // console.log(profile)
+                                                return res.status(200).send({
+                                                    msg: 'Logged in!',
+                                                    user: profile[0],
+                                                    status: '200 OK'
+                                                });
+                                            });
+                                        });
+                                    }
+                                    else {
+                                        return res.Pleasestatus(401).send({
+                                            msg: 'Include a valid OTP',
+                                            status: '401 Unauthorized'
+                                        })
+                                    }
+                                } catch (verify) {
+                                    return res.send({
+                                        TokenClient: token
+                                    });
+
+                                }
+                            }
+                            else {
                                 res.status(200).send('Profile updated successfully');
                             }
                         }
                     });
+
+
+
+                // Update the user's profile information in the database
+                // db.query(`UPDATE users, profile_user SET name = ?, profile_user.email = ?, password = ?, avatar = ?, lock_deposits = ?, lock_withdrawals = ?, maintenance = ?, close_system = ?`, [user.name, user.email, user.password, user.avatar, user.lock_deposits, user.lock_withdrawals, user.maintenance, user.close_system],
+                //     (err, result) => {
+                //         if (err) {
+                //             console.log(err);
+                //             res.status(500).send('Internal server error');
+                //         } else {
+                //             if (result.affectedRows === 0) {
+                //                 res.status(400).send('Invalid request');
+                //             } else {
+                //                 res.status(200).send('Profile updated successfully');
+                //             }
+                //         }
+                //     });
             }
         }
     });
