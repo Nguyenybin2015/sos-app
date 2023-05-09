886acee3c9d70da4cad5b744494416121a8f7d3e
Bin Nguyễn <nguyenybin2015@gmail.com>
2023-05-09 15:04:04 +0700

    update middleware check otp

--- src/routes/p2p.route.js
+++ src/routes/p2p.route.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import { normalUser, blockUser, updateUserP2PController } from '../controllers/p2p.controller.js';
 import isAuth from '../middlewares/authen-token.js';
 import { verifyOtp } from '../controllers/auth.controller.js';
+import checkOTP from '../middlewares/check-otp.js';
 
 const p2pRoutes = Router();
 
 p2pRoutes.get('/get-user-normal', [isAuth], normalUser);
 p2pRoutes.get('/get-user-blocked', [isAuth], blockUser);
-p2pRoutes.put('/update', [isAuth], verifyOtp, updateUserP2PController);
+p2pRoutes.put('/update', [isAuth, checkOTP], updateUserP2PController);
 
 export default p2pRoutes;
